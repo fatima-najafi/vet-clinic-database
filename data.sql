adb2ac082dde47fa7346422c6fbec51132fb52b9
Fatima Najafi <najafifatima762@gmail.com>
2023-09-18 17:53:36 +0430

    Insert data for animals table

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
--- Insert data into the animals' table
+--Insert data into the animals' table
 INSERT INTO animals ( id ,name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23),
        (2, 'Gabumon', '2018-11-15', 2, true, 8),
