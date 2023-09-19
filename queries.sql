e08c0e08dee88baa2b751713044bf093c8d0c95e
Fatima Najafi <najafifatima762@gmail.com>
2023-09-19 16:31:07 +0430

    Update the species and rollback the changes

--- queries.sql
+++ queries.sql
@@ -10,5 +10,14 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
            ---------------------------Change species and ROLLBAKC the CHANGES---------------------------------------
+     BEGIN;
+     UPDATE animals
+     SET species = 'unspecified'
+
+     SELECT * FROM animals;
+     ROLLBACK;
+
+    SELECT *  FROM animals;
+
 
         
