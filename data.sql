79b34d26376ae6fe87999dfb08f7ee665857b73d
Fatima Najafi <najafifatima762@gmail.com>
2023-09-19 10:43:46 +0430

    Update

--- data.sql
+++ data.sql
@@ -4,6 +4,7 @@ VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23),
        (2, 'Gabumon', '2018-11-15', 2, true, 8),
        (3,'Pikachu', '2021-01-07', 1, false, 15.04),
        (4,'Devimon', '2017-05-12', 5, true, 11);
+       
 -- insert new data into the animals' table;
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
@@ -15,4 +16,3 @@ VALUES
   (9,'Boarmon', '2005-06-07', 7, TRUE, 20.4),
   (10,'Blossom', '1998-10-13', 3, TRUE, 17),
   (11,'Ditto', '2022-05-14', 4, TRUE, 22);
-  
