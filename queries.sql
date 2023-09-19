ae554d896458b52426557553b49f1d83196c3285
Fatima Najafi <najafifatima762@gmail.com>
2023-09-19 17:33:54 +0430

    Add new Command

--- queries.sql
+++ queries.sql
@@ -40,4 +40,6 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
     ROLLBACK;
 
     SELECT * FROM animals;
+
+                                          -------Set SAVEPOINT------
     
