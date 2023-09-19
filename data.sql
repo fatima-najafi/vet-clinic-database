06d41728b094e456f687355dbeb62e3391f7fe3b
Fatima Najafi <najafifatima762@gmail.com>
2023-09-19 10:42:41 +0430

    Add new data in animals table

--- data.sql
+++ data.sql
@@ -4,3 +4,4 @@ VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23),
        (2, 'Gabumon', '2018-11-15', 2, true, 8),
        (3,'Pikachu', '2021-01-07', 1, false, 15.04),
        (4,'Devimon', '2017-05-12', 5, true, 11);
+-- insert new data into the animals' table;
