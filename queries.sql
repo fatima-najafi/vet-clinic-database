82f41ce10169c8077cd626d3d1b8885595cfeb3e
Fatima Najafi <najafifatima762@gmail.com>
2023-09-20 18:07:49 +0430

    Show who owns the most animals

--- queries.sql
+++ queries.sql
@@ -105,10 +105,10 @@ JOIN owners o ON a.owner_id = o.id
 WHERE o.full_name = 'Dean Winchester'
 AND a.escape_attempts = 0;
 
--- -- Who owns the most animals?
--- SELECT o.full_name, COUNT(*) AS animal_count
--- FROM owners o
--- JOIN animals a ON o.id = a.owner_id
--- GROUP BY o.full_name
--- ORDER BY animal_count DESC
--- LIMIT 1;                      
+-- Who owns the most animals?
+SELECT o.full_name, COUNT(*) AS animal_count
+FROM owners o
+JOIN animals a ON o.id = a.owner_id
+GROUP BY o.full_name
+ORDER BY animal_count DESC
+LIMIT 1;                      
