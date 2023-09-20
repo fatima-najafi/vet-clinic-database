e3df3b7562d5276b4a857a71e0ce21f51d9ed51a
Fatima Najafi <najafifatima762@gmail.com>
2023-09-20 17:34:20 +0430

    Add owner_id as a foreign key in animals table

--- schema.sql
+++ schema.sql
@@ -32,6 +32,7 @@ ADD COLUMN species VARCHAR(100)
   ALTER TABLE animals
   DROP COLUMN species;
   -- Modify the animals table
+  
   ALTER TABLE animals
   ADD COLUMN species_id INTEGER REFERENCES species(id),
   ADD COLUMN owner_id INTEGER REFERENCES owners(id);
