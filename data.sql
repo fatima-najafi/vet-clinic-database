0208b87a94e5c296149f5985ec98a438e4122fe5
Fatima Najafi <najafifatima762@gmail.com>
2023-09-20 17:36:40 +0430

    Insert value in owner table

--- data.sql
+++ data.sql
@@ -16,3 +16,12 @@ VALUES
   (9,'Boarmon', '2005-06-07', 7, TRUE, 20.4),
   (10,'Blossom', '1998-10-13', 3, TRUE, 17),
   (11,'Ditto', '2022-05-14', 4, TRUE, 22);
+
+--Insert the following data into the owners table:
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
