4b67e9989ef06ce38fa52dddf48dec15d5f7cad2
Fatima Najafi <najafifatima762@gmail.com>
2023-09-21 13:57:52 +0430

    Create the visits table

--- schema.sql
+++ schema.sql
@@ -52,10 +52,10 @@ CREATE TABLE specializations (
   PRIMARY KEY (vet_id, species_id)
 );
 
--- -- Create the visits join table
--- CREATE TABLE visits (
---   animal_id INTEGER REFERENCES animals(id),
---   vet_id INTEGER REFERENCES vets(id),
---   visit_date DATE,
---   PRIMARY KEY (animal_id, vet_id, visit_date)
--- );
+-- Create the visits join table
+CREATE TABLE visits (
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE,
+  PRIMARY KEY (animal_id, vet_id, visit_date)
+);
